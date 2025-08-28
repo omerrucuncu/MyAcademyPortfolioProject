099670f4ee71bd462fdc753d4a19eb4029ef196b
Ömer Üçüncü <118854548+omerrucuncu@users.noreply.github.com>
2025-08-28 12:09:31 +0300

    .

--- MyPortfolio/Controllers/CategoryController.cs
+++ MyPortfolio/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyPortfolio.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,11 @@ namespace MyPortfolio.Controllers
 {
     public class CategoryController : Controller
     {
-        // GET: Category
+        MyAcademyPortfolioProjectEntities entities = new MyAcademyPortfolioProjectEntities();
         public ActionResult Index()
         {
-            return View();
+            var values = entities.TblCategories.ToList();   
+            return View(values);
         }
     }
 }

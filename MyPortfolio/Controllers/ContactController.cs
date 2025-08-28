789c1719b3eaffb163479cbb8f4ce41f4887a024
Ömer Üçüncü <118854548+omerrucuncu@users.noreply.github.com>
2025-08-28 12:53:52 +0300

    .

---
+++ MyPortfolio/Controllers/ContactController.cs
@@ -0,0 +1,19 @@
+﻿using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        MyAcademyPortfolioProjectEntities entities = new MyAcademyPortfolioProjectEntities();
+        public ActionResult Index()
+        {
+            var values = entities.TblContacts.ToList();
+            return View(values);
+        }
+    }
+}
